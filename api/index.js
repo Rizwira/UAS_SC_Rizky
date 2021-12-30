1ef0d57f279ec729821c17ecc93f48624fbac71b
Rizwira <53108381+Rizwira@users.noreply.github.com>
2021-12-30 17:44:53 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,76 +2,63 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js');
+const model = require('./sdk/model.js'); //predict 
+
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5002726685:AAHEjvJwJdfjPImumA1GfCPHoeu3mCMbbas'
 const bot = new TelegramBot(token, {polling: true});
 
-
-// main menu bots
+state=0;
+// main menu bot
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `hello ${msg.chat.first_name}, welcome to bot_SC_Rizky\n
+        `hello ${msg.chat.first_name}, welcome...\n
         click /predict`
-    );   
+    );  
 });
 
-// input requires i and r
-state = 0;
+// input I dan r
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai y1|y2|y3 contoh 9|9|9`
+        `masukan nilai x1|x2|x3 contoh 9|9|3`
     );   
     state = 1;
 });
 
-bot.on('message', (msg) => {
+bot.on('message',(msg) =>{
     if(state == 1){
-        s = msg.text.split("| |");
-        y1 = s[0]
-        y2 = s[1]
-        y3 = s[2]
-        model.predict(
-            [
-                parseFloat(s[0]), // string to Float
-                parseFloat(s[1]),
-                parseFloat(s[2])
-            ]
-        ).then((jres)=>{
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai y1 yang diprediksi adalah ${jres[0]} y1` 
-           );
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai y2 yang diprediksi adalah ${jres[1]} y2`
-           );
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai y3 yang diprediksi adalah ${jres[2]} y3`
-           );
-       })
-    }else{
-        state = 0 
+    s= msg.text.split("|");
+    x1 = s[0]
+    x2 = s[1]
+    x3 = s[2]  
+     model.predict(
+[
+    parseFloat(s[0]), // string to float
+    parseFloat(s[1]),
+    parseFloat(s[2])
+]
+).then((jres)=>{
+    bot.sendMessage(
+         msg.chat.id,
+         `nilai x1 yang diprediksi adalah ${jres[0]} n`
+            );
+    bot.sendMessage(
+         msg.chat.id,
+          `nilai x2 yang diprediksi adalah ${jres[1]} n`
+            );
+     bot.sendMessage(
+         msg.chat.id,
+         `nilai x3 yang diprediksi adalah ${jres[2]} n`
+            );
+   })
+}else{
+state = 0;
     }
 })
-
-// routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.x1), // string to float
-            parseFloat(req.params.x2),
-            parseFloat(req.params.x3)
-        ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
-});
 
 module.exports = r;
